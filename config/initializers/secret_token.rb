f1f59fd8501abefe82f4576ffc5b9a78c3fd7fb1
Carlos Rodríguez <carlos.rodriguez@pragmagenia.com>
2014-04-17 19:32:31 -0430

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Market::Application.config.secret_key_base = 'bd2a8338b01a34464f248dde16dd37a5244e942d69e9020748c3da5ccae7f45938724c9337199d9c96ed57f0527fd18d96ee457e7e8fb169ea305d80c72e8231'
